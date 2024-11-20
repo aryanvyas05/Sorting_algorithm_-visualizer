5eeabb984c5cb00c8f5586f9aa940381fc366d97
Aryan Vyas <aryan.vyas2005@gmail.com>
2024-11-20 17:36:19 -0600

    Added insertion sort visualizer

--- visualizePygame.py
+++ visualizePygame.py
@@ -3,7 +3,7 @@ import random
 
 pygame.init()
 
-WIDTH = 1000
+WIDTH = 800
 HEIGHT = 600
 BAR_WIDTH = 3
 NUM_BARS = WIDTH // BAR_WIDTH
@@ -14,6 +14,12 @@ BLACK = (0, 0, 0)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Sorting Visualizer")
 clock = pygame.time.Clock()
+
+def draw_bars(arr):
+    screen.fill(BLACK) 
+    for i, height in enumerate(arr):
+        pygame.draw.rect(screen, WHITE, (i * BAR_WIDTH, HEIGHT - height, BAR_WIDTH, height))
+    pygame.display.update()
 
 def draw_single_bar(index, height, color=WHITE):
     pygame.draw.rect(screen, BLACK, (index * BAR_WIDTH, 0, BAR_WIDTH, HEIGHT))
@@ -33,12 +39,32 @@ def bubble_sort(arr):
         pygame.event.pump()
         if not swapped:
             break
+def insertion_sort(arr):
+    for i in range(1, (len(arr))):
+        key = arr[i]
+        j = i-1
+        while j >=0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            draw_single_bar(j + 1, arr[j + 1])
+            j -= 1
+            pygame.event.pump()
+        arr[j+1] = key
+        draw_single_bar(j + 1, arr[j + 1])
 
 def main():
     arr = random.sample(range(10, HEIGHT), NUM_BARS)
+    print("Running Bubble Sort...")
     bubble_sort(arr)
 
     pygame.time.wait(2000)
+
+    arr = random.sample(range(10, HEIGHT), NUM_BARS)
+    print("Running Insertion Sort...")
+    draw_bars(arr)
+    insertion_sort(arr)
+
+    pygame.time.wait(2000)
+
     pygame.quit()
 
 main()
